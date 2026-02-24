08dc096cb5a052986ccf2f1ace8d0267a9ec8329
mustarchild <159411807+mustarchild@users.noreply.github.com>
2026-02-24 14:52:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,86 +1,171 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.toast import toast
+from kivy.clock import Clock
 from jnius import autoclass
 from android.permissions import request_permissions, Permission
 
 layout = '''
+
 BoxLayout:
-    id: boxlayout
     orientation:'vertical'
-    adaptive_size: True
+    padding: 10
+    spacing: 10
 
     MDIconButton:
         icon: "bluetooth"
-        pos_hint: {'center_x':0.5,'center_y':0.875}
+        pos_hint: {'center_x':0.5}
         user_font_size: "40sp"
-        on_release: app.android_bluetooth.getAndroidBluetoothSocket('rasp')
+        on_release: app.connect_bluetooth()
+
+    ScrollView:
+        GridLayout:
+            id: grid
+            cols: 2
+            size_hint_y: None
+            height: self.minimum_height
+            row_default_height: 60
+            spacing: 10
+
+    MDRaisedButton:
+        text: "SAVE BMS PARAMETERS"
+        size_hint_y: None
+        height: 60
+        on_release: app.send_bms_data()
 '''
 
 class AndroidBluetoothClass:
 
-    def getAndroidBluetoothSocket(self,DeviceName):
-        toast("AndroidBluetoothSocket")
-        print("AndroidBluetoothSocket")
-        paired_devices = self.BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
-        socket = None
-        for device in paired_devices:
-            if device.getName() == DeviceName:
-                socket = device.createRfcommSocketToServiceRecord(
-                    self.UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
-                self.ReceiveData = self.BufferReader(self.InputStream(socket.getInputStream()))
-                self.SendData = socket.getOutputStream()
-                socket.connect()
-                self.ConnectionEstablished = True
-                print('Bluetooth Connection successful')
-                toast("Bluetooth Connection successful")
-
-        return self.ConnectionEstablished
-
-
-    def BluetoothSend(self, Message, *args):
-        toast("Bluetooth send")
-        print("Bluetooth send")
-        if self.ConnectionEstablished == True:
-            self.SendData.write(Message)
-        else:
-            print('Bluetooth device not connected')
-
-
-    def BluetoothReceive(self,*args):
-        toast("Bluetooth receive")
-        print("Bluetooth receive")
-        DataStream = ''
-        if self.ConnectionEstablished == True:
-            DataStream = str(self.ReceiveData.readline())
-        return DataStream
-
     def __init__(self):
-        toast("Bluetooth init")
-        print("Bluetooth init")
         self.BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
-        self.BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
-        self.BluetoothSocket = autoclass('android.bluetooth.BluetoothSocket')
         self.UUID = autoclass('java.util.UUID')
         self.BufferReader = autoclass('java.io.BufferedReader')
         self.InputStream = autoclass('java.io.InputStreamReader')
         self.ConnectionEstablished = False
-        toast("Bluetooth init finish")
-        print("Bluetooth init finish")
 
+    def connect(self, DeviceName):
+        paired_devices = self.BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
+        for device in paired_devices:
+            if device.getName() == DeviceName:
+                socket = device.createRfcommSocketToServiceRecord(
+                    self.UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+                socket.connect()
+                self.SendData = socket.getOutputStream()
+                self.ReceiveData = self.BufferReader(self.InputStream(socket.getInputStream()))
+                self.ConnectionEstablished = True
+                toast("Bluetooth Connected")
+                return True
+        toast("Device Not Found")
+        return False
 
-    def __del__(self):
-        toast("Bluetooth del")
-        print('class AndroidBluetooth destroyer')
+    def send(self, message):
+        if self.ConnectionEstablished:
+            self.SendData.write(message.encode())
+
+    def receive(self):
+        if self.ConnectionEstablished:
+            return str(self.ReceiveData.readline())
+        return None
+
 
 class TestApp(MDApp):
 
     def build(self):
-        request_permissions([Permission.BLUETOOTH_ADMIN,
-                             Permission.BLUETOOTH, Permission.BLUETOOTH_ADVERTISE ,  Permission.BLUETOOTH_CONNECT ,  Permission.BLUETOOTH_SCAN ])  # get the permissions needed
+
+        request_permissions([
+            Permission.BLUETOOTH_ADMIN,
+            Permission.BLUETOOTH,
+            Permission.BLUETOOTH_CONNECT,
+            Permission.BLUETOOTH_SCAN
+        ])
+
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Green"
-        self.android_bluetooth = AndroidBluetoothClass()
-        return Builder.load_string(layout)
+
+        self.bt = AndroidBluetoothClass()
+        root = Builder.load_string(layout)
+
+        from kivymd.uix.label import MDLabel
+        from kivymd.uix.textfield import MDTextField
+
+        self.dashboard_fields = {}
+        self.bms_fields = {}
+
+        # ------------------ DASHBOARD (READ ONLY) ------------------
+
+        dashboard_items = [
+            "Peak Voltage","Peak Current","Temperature","SOC",
+            "Cell1","Cell2","Cell3","Cell4","Cell5","Cell6","Cell7","Cell8",
+            "Cell9","Cell10","Cell11","Cell12","Cell13","Cell14","Cell15","Cell16"
+        ]
+
+        root.ids.grid.add_widget(MDLabel(text="--- DASHBOARD (READ ONLY) ---"))
+        root.ids.grid.add_widget(MDLabel(text=""))
+
+        for item in dashboard_items:
+            root.ids.grid.add_widget(MDLabel(text=item))
+            field = MDTextField(readonly=True)
+            self.dashboard_fields[item] = field
+            root.ids.grid.add_widget(field)
+
+        # ------------------ BMS PARAMETERS (SEND + RECEIVE) ------------------
+
+        bms_items = [
+            "Over Voltage Limit",
+            "Under Voltage Limit",
+            "Over Current Limit",
+            "Temperature Limit"
+        ]
+
+        root.ids.grid.add_widget(MDLabel(text="--- BMS PARAMETERS ---"))
+        root.ids.grid.add_widget(MDLabel(text=""))
+
+        for item in bms_items:
+            root.ids.grid.add_widget(MDLabel(text=item))
+            field = MDTextField()
+            self.bms_fields[item] = field
+            root.ids.grid.add_widget(field)
+
+        return root
+
+
+    # ------------------ BLUETOOTH CONNECT ------------------
+
+    def connect_bluetooth(self):
+        if self.bt.connect("rasp"):
+            Clock.schedule_interval(self.read_data, 1)
+
+
+    # ------------------ RECEIVE DATA ------------------
+
+    def read_data(self, dt):
+        data = self.bt.receive()
+        if data:
+            try:
+                values = data.strip().split(",")
+
+                keys = list(self.dashboard_fields.keys())
+                for i in range(min(len(values), len(keys))):
+                    self.dashboard_fields[keys[i]].text = values[i]
+
+            except:
+                pass
+
+
+    # ------------------ SEND ONLY BMS PARAMETERS ------------------
+
+    def send_bms_data(self):
+
+        data_string = ""
+        for key in self.bms_fields:
+            value = self.bms_fields[key].text
+            data_string += value + ","
+
+        data_string = "BMS:" + data_string[:-1] + "\n"
+
+        print("Sending:", data_string)
+        self.bt.send(data_string)
+        toast("BMS Parameters Sent")
+
 
 TestApp().run()
